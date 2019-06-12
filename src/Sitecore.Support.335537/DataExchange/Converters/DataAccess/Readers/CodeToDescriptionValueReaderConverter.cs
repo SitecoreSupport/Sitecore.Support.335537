40b1453becd2837a4dbeec9ce33d9f3c4dbe63dc
Vlad Molibozhenko <vlm@sitecore.net>
2019-06-12 09:10:26 +0800

    initial code

---
+++ src/Sitecore.Support.335537/DataExchange/Converters/DataAccess/Readers/CodeToDescriptionValueReaderConverter.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.DataExchange;
+using Sitecore.DataExchange.Attributes;
+using Sitecore.DataExchange.Converters;
+using Sitecore.DataExchange.DataAccess;
+using Sitecore.DataExchange.DataAccess.Readers;
+using Sitecore.DataExchange.Repositories;
+using Sitecore.Services.Core.Model;
+
+namespace Sitecore.Support.DataExchange.Converters.DataAccess.Readers
+{
+    [SupportedIds("{970FAE22-66D2-4F79-85ED-8C7A3FC6CF4D}")]
+    public class CodeToDescriptionValueReaderConverter : BaseItemModelConverter<IValueReader>
+    {
+        public const string FieldNameCodeDefinitionSet = "CodeDefinitionSet";
+        public const string FieldNameCodeDefinitionValue = "Value";
+        public CodeToDescriptionValueReaderConverter(IItemModelRepository repository) : base(repository)
+        {
+        }
+        protected override ConvertResult<IValueReader> ConvertSupportedItem(ItemModel source)
+        {
+            var codeSetItem = this.GetReferenceAsModel(source, FieldNameCodeDefinitionSet);
+            if (codeSetItem == null)
+            {
+                return NegativeResult(source, "The field does not reference a valid item.", $"field: {FieldNameCodeDefinitionSet}");
+            }
+            var codeItems = this.GetChildItemModels(codeSetItem);
+            var reader = new CodeToDescriptionValueReader();
+            foreach (var codeItem in codeItems)
+            {
+                var value = this.GetIntValue(codeItem, FieldNameCodeDefinitionValue);
+                reader.Values[value] = codeItem[ItemModel.ItemName].ToString();
+            }
+            return PositiveResult(reader);
+        }
+    }
+}
---
+++ src/Sitecore.Support.335537/DataExchange/DataAccess/Readers/CodeToDescriptionValueReader.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.DataExchange.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support.DataExchange.DataAccess.Readers
+{
+    public class CodeToDescriptionValueReader : IValueReader
+    {
+        public CodeToDescriptionValueReader()
+        {
+            this.Values = new Dictionary<int, string>();
+        }
+        public IDictionary<int, string> Values { get; private set; }
+        public virtual ReadResult Read(object source, DataAccessContext context)
+        {
+            if (source is int)
+            {
+                var key = (int)source;
+                if (!this.Values.ContainsKey(key))
+                {
+                    return ReadResult.PositiveResult(null, DateTime.Now);
+                }
+                return ReadResult.PositiveResult(this.Values[key], DateTime.Now);
+            }
+            return ReadResult.NegativeResult(DateTime.Now);
+        }
+    }
+}
